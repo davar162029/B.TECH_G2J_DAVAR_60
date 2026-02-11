ace0afe3e8dc3634fa7ec808db1d1d925a150f3b
davar162029 <25114cn212@glbitm.ac.in>
2026-02-11 11:48:28 +0530

    Add files via upload

---
+++ program7.c
@@ -0,0 +1,21 @@
+
+// Program 7 : 
+#include <stdio.h>
+int main () {
+int a , b  ;
+printf("Enter the no a :");
+scanf("%d" , &a);
+
+printf("Enter the no b:");
+scanf("%d" , &b);
+
+
+if(a>b)
+{
+     printf("/nBoth the no are not equal : ");
+}
+else
+{
+    printf("\nBoth the numbers are not equal");
+}
+}
