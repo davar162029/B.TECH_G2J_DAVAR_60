d948cdd120774f95bc7ec996bbaa51890497c7bb
davar162029 <25114cn212@glbitm.ac.in>
2026-02-11 12:05:15 +0530

    Add files via upload

---
+++ program11.c
@@ -0,0 +1,34 @@
+// Program 11 : Check whether a Year is Leap year or not .
+
+#include <stdio.h>
+
+int main () {
+
+int year ;
+printf("Enter the Year : ");
+scanf("%d" , &year);
+
+if(year%400==0)
+{
+    printf("%d is a leap year" , year);
+
+}
+
+else if(year%4==0)
+{
+    printf("%d is a leap year : " , year);
+
+}
+else if(year%100==0)
+{
+    printf("%d is a leap year : ",  year);
+
+}
+
+else
+{
+    printf("%d is not a leap year ", year);
+}
+
+
+}
