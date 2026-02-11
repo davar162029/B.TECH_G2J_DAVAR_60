d0d55edba78cdf5a35403f58348d1c4853791aab
davar162029 <25114cn212@glbitm.ac.in>
2026-02-11 11:41:55 +0530

    Add files via upload

---
+++ program9.c
@@ -0,0 +1,34 @@
+
+// // Program 9 : To find the greatest of the two no
+// #include <stdio.h>
+// int main () {
+// int a , b ,c ;
+// printf("Enter the no a :");
+// scanf("%d",&a);
+// printf("Enter the no b:");
+// scanf("%d",&b);
+// printf("Enter the no c :");
+// scanf("%d",&c);
+
+// if(a>b)
+// {
+//     if(a>c)
+//     {
+//         printf("a=%d is greatest" , a);
+
+//     }
+//     else
+//     {
+//         printf("c = %d is greatest" ,c);
+     
+    
+//     }
+// }
+// else if(b>c)
+// {
+//     printf("b=%d is greatest", b );
+// }
+
+
+// return 0 ;
+// }
