06a8770e09e09517e2f890b2751c715fdea52ff0
davar162029 <25114cn212@glbitm.ac.in>
2026-02-11 11:37:52 +0530

    Add files via upload

---
+++ davar/davar.c
@@ -0,0 +1,147 @@
+// #include <stdio.h>
+// #include <math.h>
+// int main () {
+
+    // printf("Hello");
+    // return 0;
+
+// int a = 5 , b = 7, c ;
+// c = a + b ;
+// printf("Addition = %d",c);
+// return 0;
+// int a ,b ,c;
+// printf("Enter two no:");
+// scanf("%d%d", &a,&b);
+// c = a + b;
+// printf("Addition =%d",c);
+// return 0;
+// int phy,maths,evs,pps,ee,sum;
+// float percent;
+
+// printf("Enter marks:");
+// scanf("%d%d%d%d%d", &phy,&maths,&evs,&pps,&ee);
+// sum = phy+maths+evs+pps+ee;
+// percent = sum/5;
+// printf("sum= %d",sum);
+// printf("percent= %f",percent);
+// return 0;
+
+// float p , r , t,si,a,ci;
+// printf("Enter the value of principle:");
+// scanf("%f",&p);
+// printf("Enter the value of rate:");
+// scanf("%f",&r);
+// printf("Enter the value of time:");
+// scanf("%f",&t);
+// si = (p*r*t)/100;
+// a= p*pow((1+r/100),t);
+// printf("si = %f ",si);
+// ci= a-p;
+// printf("\ncompund interest=%f",ci);
+// return 0;
+
+// float r , c , a , pi=3.14;
+// printf("Enter radius: ");
+// scanf("%f",&r);
+// c = 2*pi*r;
+// a = pi*r*r;
+// printf("circumference= %f" , c);
+// printf("\narea of circle= %f" , a);
+// return 0 ;
+// float t ,c,f;
+// printf("Enter the value of temperature: ");
+// scanf("%F", &t);
+// f = ((c*9/5) + 32 ) ;
+// printf("\nTemperatue in f =%f",f);
+
+// Program 6 :
+
+// int a , b ;
+// printf("Enter the no a :");
+// scanf("%d" , &a);
+
+// printf("Enter the no b:");
+// scanf("%d" , &b);
+
+// if(a==b)
+// {
+//     printf("a and b both are equal .");
+// }
+// else
+// {
+//     printf("\nBoth the numbers are not equal");
+// }
+
+//Program 7 : 
+
+
+// int a , b ,   ;
+// printf("Enter the no a :");
+// scanf("%d" , &a);
+
+// printf("Enter the no b:");
+// scanf("%d" , &b);
+
+
+// if(a>b)
+// {
+//      print("")
+// }
+// else
+// {
+//     printf("\nBoth the numbers are not equal");
+// }
+
+
+
+//Program 8 :
+
+
+// int a ;
+// printf("Enter the no : ");
+// scanf("%d",&a);
+
+// if(a%2==0)\
+// {
+//     printf("a is even no : ");
+// }
+// else{
+//     printf("\n a is odd no : ");
+// }
+
+
+
+// Program 9 :
+ 
+// int a , b ,c ;
+// printf("Enter the no a :");
+// scanf("%d",&a);
+// printf("Enter the no b:");
+// scanf("%d",&b);
+// printf("Enter the no c :");
+// scanf("%d",&c);
+
+// if(a>b)
+// {
+//     if(a>c)
+//     {
+//         printf("a=%d is greatest" , a);
+
+//     }
+//     else
+//     {
+//         printf("c = %d is greatest" ,c);
+     
+    
+//     }
+// }
+// else if(b>c)
+// {
+//     printf("b=%d is greatest", b );
+// }
+
+
+// return 0 ;
+
+
+// }
---
+++ davar/tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+
+// int a , b ;
+// printf("Enter the no a :");
+// scanf("%d" , &a);
+
+// printf("Enter the no b:");
+// scanf("%d" , &b);
+
+// if(a==b)
+// {
+//     printf("a and b both are equal .");
+// }
+// else
+// {
+//     printf("\nBoth the numbers are not equal");
+// }
